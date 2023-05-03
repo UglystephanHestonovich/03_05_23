478ceada847470e2e63a5b61926739938839faa7
UglystephanHestonovich <127040306+UglystephanHestonovich@users.noreply.github.com>
2023-05-03 09:56:48 +0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,69 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+
+dog_years = {1:14, 1.5:20, 2:24, 3:30, 4:36, 5:40, 6:42, 7:49, 8:56, 9:63, 10:65, 11:71, 12:75}
+human_years = {14:1, 20:1.5, 24:2, 30:3, 36:4, 40:5, 42:6, 49:7, 56:8, 63:9, 65:10, 71:11, 75:12}
+dog_months = {2:14, 6:5, 8:9}
+human_months = {14:2, 5:6, 9:8}
+@app.route('/lebo1/', methods=['POST'])
+def hello_world7():
+    if request.json['type'] == 'dog' and request.json['units'] == 'years':
+        a = dog_years[request.json['n']]
+        return {'n': a, 'units': 'years', 'type': 'human'}
+    elif request.json['type'] == 'human' and request.json['units'] == 'years' and request.json['n'] >= 14:
+        b = human_years[request.json['n']]
+        return {'n': b, 'units': 'years', 'type': 'dog'}
+    elif request.json['type'] == 'human' and request.json['units'] == 'month' and request.json['n'] == 14:
+        b = human_months[request.json['n']]
+        return {'n': b, 'units': 'month', 'type': 'dog'}
+    elif request.json['type'] == 'dog' and request.json['units'] == 'month' and request.json['n'] > 2:
+        a = dog_months[request.json['n']]
+        return {'n': a, 'units': 'years', 'type': 'human'}
+    elif request.json['type'] == 'dog' and request.json['units'] == 'month' and request.json['n'] == 2:
+        a = dog_months[request.json['n']]
+        return {'n': a, 'units': 'month', 'type': 'human'}
+    elif request.json['type'] == 'human' and request.json['units'] == 'years' and request.json['n'] <= 9:
+        b = human_months[request.json['n']]
+        return {'n': b, 'units': 'month', 'type': 'dog'}
+
+a = []
+@app.route('/queueclear/', methods=['POST'])
+def hello_world8():
+    a.clear()
+    if len(a) == 0:
+        return {'status': "ok"}
+
+@app.route('/queue/', methods=['POST'])
+def hello_world8_1():
+    global person
+    person = request.json["person_to_add"]
+    a.append(person)
+    print(a)
+    if len(a) > 0:
+        return {'status': "ok"}
+
+
+@app.route('/queue/', methods=['GET'])
+def hello_world8_2():
+    print(a)
+    if len(a) != 0:
+        man = a.pop(0)
+        return {'status': 'ok', 'person': man}
+    else:
+        return {'status': 'fail'}
+
+
+
+
+
+
+
+
+
+
+
+
+app.run(host="128.1.12.94", port= 5000)
